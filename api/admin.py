41efa870cae6a8d9818a3ec1ac3a496861be934c
Kutman Mairambek uulu <kutman.mairambek@noveogroup.com>
2023-01-19 17:22:15 +0300

    Setup a bit admin page

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from api.models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ['number_of_views']
+    list_display = ['id', 'title', 'text', 'created_at']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'created_at']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
