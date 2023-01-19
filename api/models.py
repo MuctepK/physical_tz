b02ba009136775211fd198437cac02397a3de657
Kutman Mairambek uulu <kutman.mairambek@noveogroup.com>
2023-01-19 11:12:40 +0300

    Database models setup

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=127)
+    text = models.TextField(max_length=2048, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    number_of_views = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=2048)
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
